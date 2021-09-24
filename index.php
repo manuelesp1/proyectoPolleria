d117d3a6c221112ec54e67ae7c5fa227bf1ba281
manuelesp <manuel2601.41@gmail.com>
2021-09-24 17:16:05 -0500

    rama secundaria

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Carbon Chicken</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link rel="stylesheet" href="views/css/normalize.css">
     <link rel="stylesheet" href="views/css/skeleton.css">
     <link rel="stylesheet" href="views/css/style.css">
@@ -17,8 +18,10 @@
    
 </head>
 <body>
-    
-    <header class="banner">
+
+  
+    <h1>rama secundaria</h1>
+    <!-- <header class="container">
 		<nav class="menu">
 			<ul class="menu-ul">
                 <li><img src="views/img/logo.png" alt="" width="80px"></li>
@@ -46,7 +49,7 @@
     
 			</ul>
 		</nav>
-	</header>
+	</header> -->
     <div class="hero">
         <div class="container"> 
             <div class="row">  
@@ -123,13 +126,9 @@
 
     <?php require_once("views/includes/footer.html"); ?>
 
-    <script src="views/js/jquery-3.5.1.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
     <script src="views/js/carrito.js"></script>
-    <script
-      src="https://code.jquery.com/jquery-3.3.1.min.js"
-      integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-      crossorigin="anonymous">
-    </script>
+
   
     
 </body>
